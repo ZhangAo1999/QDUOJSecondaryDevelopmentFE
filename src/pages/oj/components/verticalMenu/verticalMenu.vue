fcf899bb97e9cf5212e2141d878543e36bde9bb3
ZhangAo1999 <2864940860@qq.com>
2020-11-10 15:10:36 +0800

    主题色控制测试版本

---
+++ src/pages/oj/components/verticalMenu/verticalMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <Card :class="'ivu-card-' + theme" :padding="0" dis-hover>
+    <ul>
+      <slot></slot>
+    </ul>
+  </Card>
+</template>
+
+<script>
+  export default {
+    name: 'VerticalMenu'
+  }
+</script>
+
+<style scoped lang="less">
+</style>
